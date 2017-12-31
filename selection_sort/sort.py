43c1ab0b4213e38db5c44a5018ea0ac4b0850a90
Achintha Gunasekara <achintha@me.com>
2018-01-01 10:24:44 +1100

    sorting using selection sort (new list)

--- selection_sort/sort.py
+++ selection_sort/sort.py
@@ -1,8 +1,8 @@
 """ Implementation of selection sort algorithm """
 
-def selection_sort(list_to_sort):
+def selection_sort_same_list(list_to_sort):
     """
-    Sort a list using selection sort algorithm
+    Sort a list using selection sort algorithm (using the same list).
     Args:
         list_to_sort (list): Unsorted list to sort.
     Returns:
@@ -18,6 +18,24 @@ def selection_sort(list_to_sort):
     return list_to_sort
 
 
+def selection_sort_new_list(list_to_sort):
+    """
+    Sort a list using selection sort algorithm (using a new list).
+    Args:
+        list_to_sort (list): Unsorted list to sort.
+    Returns:
+        list: Sorted list.
+    """
+    sorted_list = []
+    for outter_index, outter_element in enumerate(list_to_sort):  # pylint: disable=unused-variable
+        smallest_index = outter_index
+        for inner_index in range(outter_index, len(list_to_sort)):
+            if list_to_sort[inner_index] < list_to_sort[smallest_index]:
+                smallest_index = inner_index
+        sorted_list.append(list_to_sort[smallest_index])
+    return list_to_sort
+
+
 TO_SORT = [
     [1, 9, -5, 12, 2, 3, 4, 20, 6],
     [9, 8, 7, 6, 5, 4, 3, 2, 1],
@@ -25,4 +43,8 @@ TO_SORT = [
 ]
 
 for each_list in TO_SORT:
-    print selection_sort(each_list)
+    print 'Sorting using the same list'
+    print selection_sort_same_list(each_list)
+    print 'Sorting using a new list'
+    print selection_sort_new_list(each_list)
+    print '\n'
